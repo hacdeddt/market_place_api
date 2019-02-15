bf258ecf362facddd587293f6ebced4223cbbc85
thanhdd <thanhdd@ows.com.vn>
2019-02-15 11:34:12 +0700

    Adds update action the users controller

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   namespace :api, defaults: {format: :json}, path: "/" do
     scope module: :v1,
           constraints: ApiConstraints.new(version: 1, default: true) do
-      resources :users, :only => [:show, :create]
+      resources :users, :only => [:index, :show, :create, :update]
       devise_for :users
       #get "/users/:id", to: "users#show"
     end
--- spec/controllers/api/v1/users_controller_spec.rb
+++ spec/controllers/api/v1/users_controller_spec.rb
@@ -3,6 +3,15 @@ require "rails_helper"
 RSpec.describe Api::V1::UsersController, type: :controller do
   before(:each) { request.headers["Accept"] = "application/vnd.marketplace.v1" }
 
+  describe "GET #index" do
+    before(:each) do
+      @user = FactoryGirl.create :user
+      get :index, format: :json
+    end
+
+    it { should respond_with 200 }
+  end
+
   describe "GET #show" do
     before(:each) do
       @user = FactoryGirl.create :user
@@ -48,6 +57,44 @@ RSpec.describe Api::V1::UsersController, type: :controller do
       it "renders the json errors on why the user could not be created" do
         user_response = JSON.parse(response.body, symbolize_names: true)
         expect(user_response[:errors][:email]).to include "can't be blank"
+      end
+
+      it { should respond_with 422 }
+    end
+  end
+
+  describe "PUT/PATCH #update" do
+
+    context "when is successfully updated" do
+      before(:each) do
+        @user = FactoryGirl.create :user
+        patch :update, params: { id: @user.id,
+                         user: { email: "newmail@example.com" } }, format: :json
+      end
+
+      it "renders the json representation for the updated user" do
+        user_response = JSON.parse(response.body, symbolize_names: true)
+        expect(user_response[:email]).to eql "newmail@example.com"
+      end
+
+      it { should respond_with 200 }
+    end
+
+    context "when is not created" do
+      before(:each) do
+        @user = FactoryGirl.create :user
+        patch :update, params: { id: @user.id,
+                         user: { email: "bademail.com" } }, format: :json
+      end
+
+      it "renders an errors json" do
+        user_response = JSON.parse(response.body, symbolize_names: true)
+        expect(user_response).to have_key(:errors)
+      end
+
+      it "renders the json errors on whye the user could not be created" do
+        user_response = JSON.parse(response.body, symbolize_names: true)
+        expect(user_response[:errors][:email]).to include "is invalid"
       end
 
       it { should respond_with 422 }
